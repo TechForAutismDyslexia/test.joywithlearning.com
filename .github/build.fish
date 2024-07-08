b48a10dea083cecc9d60b8e07c328bb218adaea1
brohithkr <brohithkr@gmail.com>
2024-07-08 19:24:36 +0530

    cicd

--- .github/build.fish
+++ .github/build.fish
@@ -44,39 +44,18 @@ if test "$argv[1]" = "all"
 
     set filename build.config.json
     set keys (jq 'keys[]' $filename)
-    if test ! -e site
-        mkdir site
-    else
-        rm -rf ./site/*
-    end
-    if test ! -e buildfiles
-        mkdir buildfiles
-    else
-        rm -rf ./buildfiles/*
-    end
+    # if test ! -e site
+    #     mkdir site
+    # else
+    #     rm -rf ./site/*
+    # end
+    # if test ! -e buildfiles
+    #     mkdir buildfiles
+    # else
+    #     rm -rf ./buildfiles/*
+    # end
     for i in $keys
-        set subdomain (jq -r ".[$i].subdomain" $filename)
-        set name (basename $subdomain)
-        set repolink (jq -r ".[$i].repolink" $filename)
-        set repodir ./buildfiles/$name
-        # if "$subdomain" = "/" 
-        #     set repodir "./buildfiles/root"
-        # end
-        echo $name
-        echo $repolink
-        echo $repodir
-        echo "$subdomain $repolink"
-        git clone "$repolink" "$repodir"
-        cd $repodir
-        bun i
-        bun run build
-        cd -
-        mkdir -p "./site/$subdomain"
-        if test -e $repodir/build
-            cp -r $repodir/build/* ./site/$subdomain
-        else if test -e $repodir/dist
-            cp -r $repodir/dist/* ./site/$subdomain
-        end
+        buildRepo "$i"
     end
 else
     buildRepo "$argv[1]"
